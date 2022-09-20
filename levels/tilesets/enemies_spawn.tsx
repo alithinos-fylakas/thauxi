d3a1532ca8b0eedff2673ee77387468331ed2d0f
Carlos Veras <pessoal.carlosveras@gmail.com>
2022-09-19 21:05:44 -0300

    Initial commit: everything is here

---
+++ levels/tilesets/enemies_spawn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="enemies_spawn" tilewidth="32" tileheight="32" tilecount="12" columns="4">
+ <image source="../../Imagens/pixel art/enemies_spawn.png" width="128" height="96"/>
+</tileset>
---
+++ levels/tilesets/props1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="props1" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
+ <image source="../assets/PixelFantasy_Caves_1.0/props1.png" width="1280" height="1280"/>
+</tileset>
